3abd1206e30f673d3fe87edefbb0e9e03095b454
Frida Arcadia <frida.arcadialv@gmail.com>
2025-11-14 22:57:56 -0600

    feature: patient cancels appointment

--- backend/src/routes/agenda.routes.ts
+++ backend/src/routes/agenda.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router()
 
 import getAppointmentsPerDay from "../controller/agenda/getAppointmentsPerDay.controller";
+import cancelAppointment from "src/controller/agenda/cancelAppointment.controller";
 import { authenticate } from "src/middleware/auth.middleware";
 import { requirePrivileges } from "src/middleware/rbac.middleware";
 import { Privilege } from "src/types/rbac.types";
@@ -11,5 +12,10 @@ router.get("/appointments-per-day",
     authenticate,
     requirePrivileges([Privilege.VIEW_APPOINTMENTS]),
      getAppointmentsPerDay);
+
+router.post("/appointments/:id/cancel", 
+    authenticate,
+    requirePrivileges([Privilege.UPDATE_APPOINTMENTS]),
+     cancelAppointment);
 
 export default router;
