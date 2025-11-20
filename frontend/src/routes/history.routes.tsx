7380e94bf5517a7aca4e3d8471a30de3b430534c
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2025-11-20 15:04:20 -0600

    comments in english

--- frontend/src/routes/history.routes.tsx
+++ frontend/src/routes/history.routes.tsx
@@ -3,12 +3,9 @@ import { HistoryPage } from '../pages/History/HistoryPage';
 
 export const HistoryRoutes = () => (
   <Routes>
-    {/* Esta ruta captura el ID del paciente */}
+    {/* This route gets the patient ID */}
     <Route path="/:patientId" element={<HistoryPage />} />
 
-    {/* TODO: Crear una página 'PatientListPage' que muestre
-        una lista de pacientes y navegue a la ruta de arriba.
-        <Route path="/" element={<PatientListPage />} /> 
-    */}
+    {}
   </Routes>
 );
