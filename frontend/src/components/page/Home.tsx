ca52e82aada1984703c77a7bfbb4c29df0c97c19
Manu090704 <A01711581@tec.mx>
2025-11-20 15:00:29 -0600

    add: button

--- frontend/src/components/page/Home.tsx
+++ frontend/src/components/page/Home.tsx
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold">Home</h1>
+      <a href="/login" className="mt-4 text-blue-500 underline">Go to Lab Results</a>
     </div>
   );
 };
