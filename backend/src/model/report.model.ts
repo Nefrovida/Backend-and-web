f337925a935c87114584c77d7ed21b288c025703
Manu090704 <A01711581@tec.mx>
2025-11-21 23:26:17 -0600

    remove: comments

--- backend/src/model/report.model.ts
+++ backend/src/model/report.model.ts
@@ -19,24 +19,22 @@ export default class Report {
   }
 
   static async getResultsByUserId(userId: string) {
-  // 1. Buscar el paciente por el user_id
+  
   const patient = await prisma.patients.findFirst({
     where: { user_id: userId },
   });
 
   if (!patient) return [];
 
-  // 2. Buscar TODOS los análisis del paciente
+
   const analysis = await prisma.patient_analysis.findMany({
     where: { patient_id: patient.patient_id },
   });
 
   if (analysis.length === 0) return [];
 
-  // 3. Extraer todos los IDs de patient_analysis
   const analysisIds = analysis.map(a => a.patient_analysis_id);
 
-  // 4. Buscar TODOS los resultados asociados a esos analysis
   return prisma.results.findMany({
     where: {
       patient_analysis_id: { in: analysisIds },
