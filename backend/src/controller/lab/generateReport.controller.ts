dd0aa72f45a69ce63c0532cff56fea9a30e5746f
Andre Agle <andrezala03@gmail.com>
2025-11-24 13:04:58 -0600

    add size check to backend

--- backend/src/controller/lab/generateReport.controller.ts
+++ backend/src/controller/lab/generateReport.controller.ts
@@ -4,18 +4,20 @@ import * as generateLabReportService from "#/src/service/lab/lab.service";
 
 async function generateLabReport(req: Request, res: Response) {
     try {
-        // console.log("In generate lab report controller");
-        // console.log("req body: ", req.body);
         const { patient_analysis_id, interpretations, recommendations } = req.body;
         const id = Number(patient_analysis_id)
+
+        if (interpretations.length > 400 || recommendations.length > 400) {
+            return res.status(400).json({ success: false, message: "Interpretaciones y recomendaciones no pueden exceder 400 caracteres" });
+        }
 
         // Update recommendations/interpretations values in 'results' model object
         const result = await generateLabReportService.generateLabReport(id, interpretations, recommendations);
         
-        res.status(200).json({ success: true, message: "Report submitted successfully" });
+        res.status(200).json({ success: true, message: "Reporte subido con éxito" });
     } catch (error: any) {
-        console.error("Error generating lab report: ", error);
-        res.status(500).json({ success: false, message: "Server error generating the lab report" });
+        console.error("Error al generar el reporte de laboratorio: ", error);
+        res.status(500).json({ success: false, message: "Error del servidor al generar reporte" });
     }
 }
 
