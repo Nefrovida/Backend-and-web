90e910e3878cfd9fb9e7c945c1dfa82a8444ddc4
Ivan <upp.estancia@gmail.com>
2025-11-24 14:54:16 -0600

    chore: english comments

--- frontend/src/components/page/LabUploadPage.tsx
+++ frontend/src/components/page/LabUploadPage.tsx
@@ -1,10 +1,8 @@
-// src/components/pages/LabAppointmentPage.tsx (o como se llame)
 import PendingLabAppointmentsList from "@/components/organism/lab/PendingLabAppointmentsList";
 import LabAppointmentUpload from "@/components/organism/lab/LabAppointmentUpload";
 
 function LabAppointmentPage() {
     return (
-        // si tienes navbar fijo arriba de 4rem, adapta el calc
         <div className="flex h-[calc(100vh-4rem)] overflow-hidden">
             <PendingLabAppointmentsList />
             <div className="flex-1 h-full overflow-y-auto">
