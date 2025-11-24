eeb3ad9ba1a7c8b8b49973298d81c2f61d262b27
Andre Agle <andrezala03@gmail.com>
2025-11-24 13:25:26 -0600

    filter goes away if you click off

--- frontend/src/components/atoms/Filter.tsx
+++ frontend/src/components/atoms/Filter.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useRef, useEffect } from 'react'
 import { BsFilter, BsX } from 'react-icons/bs'
 
 interface Props {
@@ -8,10 +8,27 @@ interface Props {
 
 const Filter: FC<Props> = ({show}) => {
   const [modal, setModal] = useState(false)
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      // If we clicked outside the component, close it
+      if (wrapperRef.current && !wrapperRef.current.contains(e.target as Node)) {
+        setModal(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
 
 
   return (<>
-    <div className={`w-24 rounded-full bg-white flex justify-between items-center px-2 cursor-pointer ${modal ? 'bg-red-600 hover:bg-red-500' : 'hover:bg-gray-100'}`}
+    <div 
+      ref={wrapperRef}
+      className={`w-24 rounded-full bg-white flex justify-between items-center px-2 cursor-pointer ${modal ? 'bg-red-600 hover:bg-red-500' : 'hover:bg-gray-100'}`}
       onClick={() => setModal(prev => !prev)}>
         <p>Filter</p>
         {modal ? <BsX/> : <BsFilter />}
