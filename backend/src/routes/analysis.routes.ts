80a4ec1d7807b5eb268da83310a63a84dae3c7d9
Ricardo Calzada <a01707647@tec.mx>
2025-11-26 21:41:36 -0600

    New analysis routes and controller into analysis carpet

---
+++ backend/src/routes/analysis.routes.ts
@@ -0,0 +1,32 @@
+import express from "express";
+const router = express.Router();
+
+import getAnalysisPerDayByAnalysisId from "../controller/analysis/getAnalysisPerDayByAnalysisId.controller";
+import createAnalysisAppointment from "../controller/analysis/createAnalysisAppointment.controller";
+import { downloadReport } from "../controller/analysis/downloadReport.controller";
+import { authenticate } from "src/middleware/auth.middleware";
+import { requirePrivileges } from "src/middleware/rbac.middleware";
+import { Privilege } from "../types/rbac.types";
+
+router.get(
+    "/analysis-per-day/by-analysis",
+    authenticate,
+    requirePrivileges([Privilege.VIEW_ANALYSIS]),
+    getAnalysisPerDayByAnalysisId
+);
+
+router.get(
+    "/download-report",
+    authenticate,
+    requirePrivileges([Privilege.VIEW_ANALYSIS]),
+    downloadReport
+);
+
+router.post(
+    "/analysis-appointment",
+    authenticate,
+    requirePrivileges([Privilege.CREATE_APPOINTMENTS]),
+    createAnalysisAppointment
+);
+
+export default router;
