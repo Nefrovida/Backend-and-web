ad6d73dfb32627856e41afa1565bb2008be92aa1
SALV <a01707122@tec.mx>
2025-12-01 17:51:07 -0600

    fix: delete backend unnecesary backend console.log

--- backend/src/controller/agenda/getAppointmentsPerDaySec.controller.ts
+++ backend/src/controller/agenda/getAppointmentsPerDaySec.controller.ts
@@ -1,23 +1,23 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Agenda from "../../model/agenda.model";
 
-
 async function getAppointmentsPerDaySec(req: Request, res: Response) {
-    try {
-        const targetDate = req.query.date as string;
-        if (!targetDate) {
-            return res.status(400).json({ error: "Date parameter is required (e.g., ?date=2023-10-05)" });
-        }
-        const appointmentsPerDay = await Agenda.getAppointmentsPerDaySec(targetDate);
-        
-        console.log(appointmentsPerDay);
+  try {
+    const targetDate = req.query.date as string;
+    if (!targetDate) {
+      return res
+        .status(400)
+        .json({ error: "Date parameter is required (e.g., ?date=2023-10-05)" });
+    }
+    const appointmentsPerDay = await Agenda.getAppointmentsPerDaySec(
+      targetDate
+    );
 
-        res.json(appointmentsPerDay);
-    }
-    catch (error){
-        console.error("Error fetching appointments:", error);
-        res.status(500).json({error: "Failed to fetch appointments"});
-    }
+    res.json(appointmentsPerDay);
+  } catch (error) {
+    console.error("Error fetching appointments:", error);
+    res.status(500).json({ error: "Failed to fetch appointments" });
+  }
 }
 
 export default getAppointmentsPerDaySec;
