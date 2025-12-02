286f161298a2df3e126e54a266faa7f9a59dd428
SALV <a01707122@tec.mx>
2025-12-02 13:11:20 -0600

    feat: Message validation / limit and loading state

--- frontend/src/components/organism/forum/SendReply.tsx
+++ frontend/src/components/organism/forum/SendReply.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from "react";
+import Loading from "@/components/molecules/Loading";
+import React, { FC, useEffect, useState } from "react";
 import { VscSend } from "react-icons/vsc";
 
 interface Props {
@@ -8,10 +9,24 @@ interface Props {
 
 const SendReply: FC<Props> = ({ replyInfo, refresh }) => {
   const [message, setMessage] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  function handleChange(content: string) {
+    if (content.length < 5000) setMessage(content);
+    else {
+      setMessage(content.substring(0, 5000));
+      setError("El mensaje no puede exceder 5000 carácteres");
+    }
+  }
+  useEffect(() => {
+    if (message.length < 50000) setError(null);
+  }, [message]);
 
   function handleSendMessage(e) {
     e.preventDefault();
-    if (!message) return;
+    if (!message || error || loading) return;
+    setLoading(true);
     const messageInfo = {
       parent_message_id: replyInfo.messageId,
       content: message,
@@ -32,29 +47,40 @@ const SendReply: FC<Props> = ({ replyInfo, refresh }) => {
         console.log(data);
         refresh((prev) => !prev);
         setMessage("");
+        setLoading(false);
       })
       .catch((e) => console.log(e));
   }
 
   return (
-    <form
-      className="shadow-sm shadow-gray-400 bg-gray-200 w-11/12 mr-60 rounded-md h-16 absolute bottom-5 flex items-center"
-      onSubmit={handleSendMessage}
-    >
-      <input
-        type="text"
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
-        className="w-full rounded-md inset-6 bg-white h-12 ml-2 align-text-top p-1"
-        placeholder="Escribir..."
-      />
-      <button
-        type="submit"
-        className="bg-blue-600 hover:bg-blue-700 text-white m-4 w-12 h-12 rounded-md flex items-center justify-center text-2xl"
+    <>
+      <form
+        className={`shadow-sm shadow-gray-400 bg-gray-200 w-11/12 mr-60 rounded-md h-16 absolute  flex items-center ${
+          error ? "bg-red-500 bottom-10" : "bottom-5"
+        }`}
+        onSubmit={handleSendMessage}
       >
-        <VscSend />
-      </button>
-    </form>
+        <input
+          type="text"
+          onChange={(e) => handleChange(e.target.value)}
+          value={message}
+          className={`w-full rounded-md inset-6 bg-white h-12 ml-2 align-text-top p-1`}
+          placeholder="Escribir..."
+          min="1"
+          max="5000"
+        />
+
+        <button
+          type="submit"
+          className={`bg-blue-600 hover:bg-blue-700 text-white m-4 w-12 h-12 rounded-md flex items-center justify-center text-2xl ${
+            loading && "bg-gray-500"
+          }`}
+        >
+          {loading ? <Loading /> : <VscSend />}
+        </button>
+      </form>
+      {error && <p className="text-red-500 absolute bottom-4">{error}</p>}
+    </>
   );
 };
 
