766c24e09498d942b7b7c056cda750d1d1fdb3c9
Ricardo Calzada <a01707647@tec.mx>
2025-12-03 05:07:20 -0600

    fix: Maternal lastname now has |null in order to fix bug errors

--- backend/src/types/profile.types.ts
+++ backend/src/types/profile.types.ts
@@ -2,7 +2,7 @@ export interface UserProfileDTO {
     user_id: string;
     name: string;
     parent_last_name: string;
-    maternal_last_name: string;
+    maternal_last_name: string | null;
     username: string;
     phone_number: string;
     role_name: string;
@@ -13,7 +13,7 @@ export interface UserProfileDTO {
 export interface UpdateProfileDTO {
     name?: string;
     parent_last_name?: string;
-    maternal_last_name?: string;
+    maternal_last_name?: string | null;
     phone_number?: string;
     gender?: string;
     birthday?: string; // ISO date string YYYY-MM-DD
